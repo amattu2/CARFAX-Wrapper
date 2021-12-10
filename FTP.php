79ea06e2eb4514cb1af2da72834c90e382793c07
Alec M <alecmattu1234@gmail.com>
2021-12-10 08:44:11 -0500

    Fix static class strings Add writeHeader and supporting functions

--- FTP.php
+++ FTP.php
@@ -9,6 +9,9 @@
 // Class Namespace
 namespace amattu\CARFAX;
 
+// Exceptions
+class FileExistsException extends \Exception {}
+
 /**
  * This is a CARFAX VHR service history FTP integration helper class
  */
@@ -18,21 +21,60 @@ class FTP {
    * 
    * @var string
    */
-  private static $host = "data.carfax.com";
+  private const HOST = "data.carfax.com";
+
+  /**
+   * Report File Header Fields
+   * 
+   * @var array
+   */
+  private const HEADER_FIELDS = [
+    "VIN",
+    "RO_OPEN_DATE", /* MM/DD/YYYY */
+    "RO_CLOSE_DATE", /* MM/DD/YYYY */
+    "MILEAGE",
+    "ODOMETER_MEASURE", /* MI OR KM */
+    "RO_INVOICE_NUMBER",
+    "SERVICE_DESCRIPTION",
+    "LABOR_DESCRIPTION",
+    "PART_NAME_DESCRIPTION",
+    "PART_QUANTITY",
+    "MAKE",
+    "MODEL",
+    "MODEL_YEAR",
+    "PLATE",
+    "PLATE_STATE",
+    "MANAGEMENT_SYSTEM", /* TBD */
+    "LOCATION_ID",
+    "LOCATION_NAME",
+    "ADDREES",
+    "CITY",
+    "STATE",
+    "POSTAL_CODE",
+    "PHONE",
+    "URL",
+  ];
+
+  /**
+   * CARFAX Partner Name
+   * 
+   * @var string
+   */
+  private $partner_name = "";
 
   /**
    * CARFAX FTP username
    * 
    * @var string
    */
-  private static $username = "";
+  private $username = "";
 
   /**
    * CARFAX FTP password
    * 
    * @var string
    */
-  private static $password = "";
+  private $password = "";
 
   /**
    * Data Report Type
@@ -45,7 +87,14 @@ class FTP {
    * 
    * @var \DateTime
    */
-  private static $date;
+  private $date = null;
+
+  /**
+   * Data Report File Handle
+   * 
+   * @var resource
+   */
+  private $handle = null;
 
   /**
    * Class Constructor
@@ -68,5 +117,58 @@ class FTP {
 
     // Set report date
     $this->date = $date ?: new \DateTime();
+  }
+
+  /**
+   * Write Report File Header into File
+   * 
+   * @param None
+   * @return bool
+   * @throws FileExistsException
+   * @author Alec M.
+   */
+  private function writeHeader() : bool
+  {
+    // Generate filename
+    $filename = $this->generateFileName();
+
+    // Check if file exists
+    if (file_exists($filename)) {
+      return true;
+    }
+
+    // Generate new file
+    $this->handle = fopen($filename, "w");
+
+    // Write header
+    fputcsv($this->handle, self::HEADER_FIELDS, "|", '"');
+
+    // Return
+    return true;
+  }
+
+  /**
+   * Generate Report File Name using Date and Type
+   * 
+   * Notes:
+   *    (1) The file name is formatted as:
+   *       <partner_name>_<type>_RO_<MMDDYYYY>.txt
+   *       PartnerName_DataStatus_DataType_FileExportDate.txt 
+   * 
+   * @param None
+   * @return string
+   * @throws None
+   * @author Alec M.
+   */
+  private function generateFileName() : string
+  {
+    // Return
+    return sprintf("%s_%s_RO_%s%s%s.txt",
+      $this->partner_name,
+      $this->type,
+      $this->date->format("m"),
+      $this->date->format("d"),
+      $this->date->format("Y")
+    );
   }
 }
