95071cb3753b5eacd08050f8f5f4a1df90189934
Alec M <alecmattu1234@gmail.com>
2021-12-10 08:04:54 -0500

    Add class constructor

--- FTP.php
+++ FTP.php
@@ -13,5 +13,60 @@ namespace amattu\CARFAX;
  * This is a CARFAX VHR service history FTP integration helper class
  */
 class FTP {
-    
+  /**
+   * VHR FTP host
+   * 
+   * @var string
+   */
+  private static $host = "data.carfax.com";
+
+  /**
+   * CARFAX FTP username
+   * 
+   * @var string
+   */
+  private static $username = "";
+
+  /**
+   * CARFAX FTP password
+   * 
+   * @var string
+   */
+  private static $password = "";
+
+  /**
+   * Data Report Type
+   * 
+   * @var string ("HIST" or "PROD")
+   */
+
+  /**
+   * Data Report Date
+   * 
+   * @var \DateTime
+   */
+  private static $date;
+
+  /**
+   * Class Constructor
+   * 
+   * @param string $username
+   * @param string $password
+   * @param ?string $type
+   * @param ?DateTime $date
+   * @throws None
+   * @author Alec M.
+   */
+  public function __construct(string $username, string $password, string $type = "PROD", \DateTime $date = null)
+  {
+    // Set connection details
+    $this->username = $username;
+    $this->password = $password;
+
+    // Set report type
+    $this->type = $type === "HIST" ? "HIST" : "PROD";
+
+    // Set report date
+    $this->date = $date ?: new \DateTime();
+  }
 }
